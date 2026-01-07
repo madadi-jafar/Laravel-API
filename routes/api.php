f10be2b50a7734ae2465e20868d077f519168977
Jafar Madadi <jafar.madadi76@gmail.com>
2026-01-07 12:31:45 +0430

    working with postman and api routes and http requests - done

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
