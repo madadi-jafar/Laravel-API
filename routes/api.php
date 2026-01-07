3af4720033faef2d303dad81557ffea4d023e2ef
Jafar Madadi <jafar.madadi76@gmail.com>
2026-01-07 11:40:03 +0430

    status 200 - done

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/login', [AuthController::class, 'login']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+ 
